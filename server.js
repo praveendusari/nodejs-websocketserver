bae69eb7794539a5c92af0baa040d04d882a3052
praveen <praveen.dusari@gmail.com>
2025-03-15 17:58:14 +0530

    websocket server

---
+++ server.js
@@ -0,0 +1,15 @@
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function message(data) {
+    if(data) {
+      //console.log('received: %s', data);
+      ws.send(`something received ${data}`);
+    }
+  });
+
+
+  ws.send('something');
+});
