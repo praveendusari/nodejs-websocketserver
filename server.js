06c5ad623660d7e2d29dfe9273cd72b2453685c8
praveen <praveen.dusari@gmail.com>
2025-03-15 19:59:50 +0530

    breadcast to all clients

--- server.js
+++ server.js
@@ -4,25 +4,15 @@ const wss = new WebSocketServer({ port: 3000 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
-    if(data) {
-      //console.log('received: %s', data);
-      ws.send(`${data}`);
+    if (data) {
+      // Broadcast the message to all clients
+      wss.clients.forEach(function each(client) {
+        if (client.readyState === client.OPEN) {
+          client.send(`${data}`);
+        }
+      });
     }
   });
+
   ws.send('something');
-});
-
-fetch("http://nodejs-websocketserver.onrender.com").then(() => {
-  const wss = new WebSocketServer({ port: 8080 });
-
-  wss.on('connection', function connection(ws) {
-    ws.on('message', function message(data) {
-      if(data) {
-        //console.log('received: %s', data);
-        ws.send(`${data}`);
-      }
-    });
-    ws.send('something');
-  });
-  
 });
