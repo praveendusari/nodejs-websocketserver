d30e2e3f79f86730b942da6a133b2da7c1a1735a
praveen <praveen.dusari@gmail.com>
2025-03-15 19:25:33 +0530

    new changes

--- server.js
+++ server.js
@@ -9,7 +9,20 @@ wss.on('connection', function connection(ws) {
       ws.send(`${data}`);
     }
   });
-
-
   ws.send('something');
 });
+
+fetch("/").then(() => {
+  const wss = new WebSocketServer({ port: 8080 });
+
+  wss.on('connection', function connection(ws) {
+    ws.on('message', function message(data) {
+      if(data) {
+        //console.log('received: %s', data);
+        ws.send(`${data}`);
+      }
+    });
+    ws.send('something');
+  });
+  
+};
